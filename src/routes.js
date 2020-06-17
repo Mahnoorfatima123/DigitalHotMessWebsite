3c16f06c17b042816f067e1b4718b3b0cc7004c2
Mahnoor <mahnoorfati8@gmail.com>
2020-06-17 14:35:25 +0500

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+import Dashboard from "views/Dashboard.jsx";
+import UserProfile from "views/UserProfile.jsx";
+import TableList from "views/TableList.jsx";
+import Typography from "views/Typography.jsx";
+import Icons from "views/Icons.jsx";
+import Maps from "views/Maps.jsx";
+import Notifications from "views/Notifications.jsx";
+
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "pe-7s-graph",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/user",
+    name: "User Profile",
+    icon: "pe-7s-user",
+    component: UserProfile,
+    layout: "/admin"
+  },
+
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: "pe-7s-map-marker",
+    component: Maps,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "pe-7s-bell",
+    component: Notifications,
+    layout: "/admin"
+  }
+];
+
+export default dashboardRoutes;
